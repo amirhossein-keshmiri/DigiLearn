e1df6ebb6ccd7ba1fb249103f50f0115a44635a8
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2025-12-03 00:14:39 +0330

    feat(admin/courses): add category management pages
    - Implement CRUD operations for course categories
    - Add pages for listing, adding and editing categories
    - Create submit button tag helper for forms
    - Support hierarchical categories with parent-child relationships

---
+++ src/EndPoints/DigiLearn.Web/Areas/Admin/Pages/Courses/Categories/Add.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using Common.Domain.Utils;
+using CoreModule.Application.Categories.AddChild;
+using CoreModule.Application.Categories.Create;
+using CoreModule.Facade.Categories;
+using DigiLearn.Web.Infrastructure.RazorUtils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace DigiLearn.Web.Areas.Admin.Pages.Courses.Categories
+{
+  [BindProperties]
+  public class AddModel : BaseRazor
+  {
+    private ICourseCategoryFacade _categoryFacade;
+
+    public AddModel(ICourseCategoryFacade categoryFacade)
+    {
+      _categoryFacade = categoryFacade;
+    }
+
+    [Display(Name = "Title")]
+    [Required(ErrorMessage = "Please Enter {0}")]
+    public string Title { get; set; }
+
+    [Display(Name = "Slug")]
+    [Required(ErrorMessage = "Please Enter {0}")]
+    public string Slug { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPost([FromQuery] Guid? parentId)
+    {
+      if (parentId != null)
+      {
+        var result = await _categoryFacade.AddChild(new AddChildCategoryCommand()
+        {
+          Title = Title,
+          Slug = Slug.ToSlug(),
+          ParentCategoryId = (Guid)parentId
+        });
+
+        return RedirectAndShowAlert(result, RedirectToPage("Index"));
+      }
+      else
+      {
+        var result = await _categoryFacade.Create(new CreateCategoryCommand
+        {
+          Title = Title,
+          Slug = Slug.ToSlug()
+        });
+
+        return RedirectAndShowAlert(result, RedirectToPage("Index"));
+      }
+
+    }
+
+  }
+}
---
+++ src/EndPoints/DigiLearn.Web/Areas/Admin/Pages/Courses/Categories/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using CoreModule.Facade.Categories;
+using CoreModule.Query._DTOs;
+using DigiLearn.Web.Infrastructure.RazorUtils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigiLearn.Web.Areas.Admin.Pages.Courses.Categories
+{
+    public class IndexModel : BaseRazor
+    {
+        private readonly ICourseCategoryFacade _categoryFacade;
+
+        public IndexModel(ICourseCategoryFacade categoryFacade)
+        {
+            _categoryFacade = categoryFacade;
+        }
+
+        public List<CourseCategoryDto> Categories { get; set; }
+        public async Task OnGet()
+        {
+            Categories = await _categoryFacade.GetMainCategories();
+        }
+
+        public async Task<IActionResult> OnPostDelete(Guid id)
+        {
+            return await AjaxTryCatch(() => _categoryFacade.Delete(id));
+        }
+    }
+}
